0eb232b5b1a4fb39d7269398fda6170f2f385dcb
lovisgod <olifedayo94@gmail.com>
2019-07-14 00:57:26 +0100

    FT/profile

--- app/src/main/java/com/lovisgod/alcchallenge/AboutAlcActivity.java
+++ app/src/main/java/com/lovisgod/alcchallenge/AboutAlcActivity.java
@@ -2,7 +2,11 @@ package com.lovisgod.alcchallenge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.http.SslError;
 import android.os.Bundle;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class AboutAlcActivity extends AppCompatActivity {
 
@@ -11,5 +15,18 @@ public class AboutAlcActivity extends AppCompatActivity {
         setTitle("About ALC");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_alc);
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+        //Enabling javascript
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://andela.com/alc/");
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                //handle the error
+                handler.proceed();
+            }
+        });
     }
 }
